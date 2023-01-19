c2f76b50153971df5e9508802a10d9b1318b1249
Park <pti0904@naver.com>
2023-01-19 12:23:21 +0900

    error but...

---
+++ lib/sample_ads_unit.dart
@@ -0,0 +1,4 @@
+// ignore_for_file: constant_identifier_names
+
+const String BANNER_TEST = 'ca-app-pub-3940256099942544/6300978111';
+const String INTERSTITIAL_TEST = 'ca-app-pub-3940256099942544/1033173712';
